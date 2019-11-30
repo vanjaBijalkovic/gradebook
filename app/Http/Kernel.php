c8db36a27833315cb846ac700771ec8ae9036f55
nenadpaic89 <nenadpaic@gmail.com>
2019-11-30 16:44:17 +0100

    added cors support

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class
     ];
 
     /**
@@ -64,7 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
-        
+
     ];
 
     /**
